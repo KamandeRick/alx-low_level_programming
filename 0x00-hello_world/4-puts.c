d749977e139c14657ca292494b1e28e13b5f302f
KamandeRick <rickmwangi237@gmail.com>
2023-02-16 14:22:48 -0800

    remove whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main -  print a line with puts function
- * Return: 0 
- */
+ * Return: 0
+ * /
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0); 
 }
