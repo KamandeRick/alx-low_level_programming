c84bf7fc87f88bc30fbfd7cf818f73d9607c3163
KamandeRick <rickmwangi237@gmail.com>
2023-02-16 13:21:51 -0800

    use puts to prit message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * return: 0
+ */
+int main(void)
+{
+	char Mystr[] = "Programming is like building a multilingual puzzle\n";
+	puts(Mystr);
+
+	return 0;
+}
