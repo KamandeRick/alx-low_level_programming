9a647acab2de5003edfc04d69ee015a29795df09
KamandeRick <rickmwangi237@gmail.com>
2023-02-21 08:09:14 -0800

    add program that prints 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	return (0);
+}
