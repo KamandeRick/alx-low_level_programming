247c2fbbf51027f1ce6748f7bc5d2361eb545fa1
KamandeRick <rickmwangi237@gmail.com>
2023-02-21 07:19:14 -0800

    add betty format

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabet with new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char lowerCase;
+
+	for (lowerCase = 'a'; lowerCasee <= z; lowerCase++)
+	{
+		putchar(laowerCase);
+	}
+	putchar('\n');
+	return (0);
+
+}
