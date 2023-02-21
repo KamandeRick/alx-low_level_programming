3547ff9a6456c2752230f84b7e7c70ff38e83c63
KamandeRick <rickmwangi237@gmail.com>
2023-02-21 05:01:00 -0800

    script with print values for n

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ */
+
+int main() {
+
+	int n;
+	printf ("%d", n);
+	if (n>0){
+		printf("is positive")
+		}
+	else (n==0) {
+		printf("is zero")
+	}
+	else (n<0) {
+		printf("is negative")};
+
+printf("\n")
