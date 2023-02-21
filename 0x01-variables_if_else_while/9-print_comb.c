b3fd1641de647d0b6599841cf6a2b2cd12763563
KamandeRick <rickmwangi237@gmail.com>
2023-02-21 10:12:04 -0800

    fix syntax

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,15 @@ int main(void)
 
 	for (num = 0; num <= 9; num++)
 	{
+		putchar(num);
 		if (num != 9)
 		{
-		putchar(num);
 		putchar(',');
 		putchar(' ');
 		}
 		else
 		putchar(num);
 	}
+	putchar('\n');
 	return (0);
 }
