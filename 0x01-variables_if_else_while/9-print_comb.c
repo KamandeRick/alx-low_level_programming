e047d91a3b47bd273ed40cdaad124e4bb87c3f3d
KamandeRick <rickmwangi237@gmail.com>
2023-02-21 10:33:44 -0800

    arrange for function

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,13 @@ int main(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		putchar("%d", num);
 		if (num != 9)
 		{
-		putchar(',');
-		putchar(' ');
+		putchar(num);
+		putchar(", ");
 		}
+		else
+		putchar(num)
 	}
 	putchar('\n');
 	return (0);
