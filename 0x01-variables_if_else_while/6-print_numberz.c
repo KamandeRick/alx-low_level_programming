73ba1c238eacfa0d39b4b8b001c116f991b7dc76
KamandeRick <rickmwangi237@gmail.com>
2023-02-21 08:34:00 -0800

    add ++ to give statement effect

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num)
-		putchar ("%d",num);
+	for (num = 0; num <= 9; num++)
+		putchar ("%d", num);
 
 	putchar("\n");
-	return 0;
+	return (0);
 }
