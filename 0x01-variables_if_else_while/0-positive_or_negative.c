4f66ee4150aae25fe0ae3e9fbda8de791401af2b
KamandeRick <rickmwangi237@gmail.com>
2023-02-21 05:16:24 -0800

    add else if block

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,13 +9,13 @@ int main() {
 
 	int n;
 	printf ("%d", n);
-	if (n>0){
+	if (n > 0){
 		printf("is positive");
 		}
-	else (n==0) {
+	else if(n == 0) {
 		printf("is zero");
 	}
-	else (n<0) {
+	else (n < 0) {
 		printf("is negative");}
 
 printf("\n")
