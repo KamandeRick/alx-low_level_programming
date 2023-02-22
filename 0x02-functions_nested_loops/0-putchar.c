db2213d081ae542f26c9c897e2e0b224ef1b4ddf
KamandeRick <rickmwangi237@gmail.com>
2023-02-22 14:31:46 -0800

    use multiple putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
- * _putchar - prints _putchar
+ * main - Entry
  * Return: Always 0
  */
 
-char message[] = "_putchar";
-
-int _putchar(void)
-
+int main(void)
 {
-	putchar(message);
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
