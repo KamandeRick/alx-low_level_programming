cdd24630a7715778f96d12845acfe528a9839418
KamandeRick <rickmwangi237@gmail.com>
2023-02-23 14:03:45 -0800

    add program to print diagonal

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,12 +11,14 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 15, j++)
+		for (j = 0; j < 15; j++)
 		{
-			if (j >= 10)
+			if (i <= 10)
+			{
 				_putchar(j / 10 + '0');
-				_putchar(j % 10 = '0');
+				_putchar(j % 10 + '0');
+			}
 		}
-	putchar('\n');
+	_putchar('\n');
 	}
 }
