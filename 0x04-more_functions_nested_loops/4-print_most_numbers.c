78efda5cea6ed70c0202fef15665e0ad9b2c82b5
KamandeRick <rickmwangi237@gmail.com>
2023-02-23 08:39:01 -0800

    add continue in program to skip 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,14 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48; i > 58; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if (i != 50 && i != 52)
+		if (i == 50 || i == 52)
 		{
-			_putchar(i);
+			continue;
 		}
+		else
+			_putchar(i);
 	}
 	_putchar('\n');
 }
