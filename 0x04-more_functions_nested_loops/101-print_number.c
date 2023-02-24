4f4abb97c6a11ca45441091f480b172cf8de8c71
root <rickmwangi237@gmail.com>
2023-02-23 17:17:22 -0800

    add program to print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,12 +14,12 @@ void print_number(int n)
 		_putchar('-');
 	} else
 	{
-		n1 = n;
+		i = n;
 	}
-	if (n1 / 10)
+	if (i / 10)
 	{
-		print_number(n1 / 10);
+		print_number(i / 10);
 	}
-	_putchar((n1 % 10) + '0');
+	_putchar((i % 10) + '0');
 }
 
