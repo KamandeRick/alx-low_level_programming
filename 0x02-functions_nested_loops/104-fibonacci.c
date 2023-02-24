c8d56ead7bec1619ac902cc29697f5d2458131b4
root <rickmwangi237@gmail.com>
2023-02-24 05:01:51 -0800

    program to print first 98 fib nos

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - 
+ *
+ * Purpose - no hardcode
+ *
+ * Return:  (Success)
+ */
+
+int main(void)
+{
+	unsigned long int i;
+	unsigned long int pre = 1;
+	unsigned long int post = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int pre1;
+	unsigned long int pre2;
+	unsigned long int post1;
+	unsigned long int post2;
+
+	printf("%lu", pre);
+
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", post);
+		post += pre;
+		pre = post - pre;
+	}
+
+	pre1 = (pre / l);
+	pre2 = (pre % l);
+	post1 = (aft / l);
+	post2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", post1 + (aft2 / l));
+		printf("%lu", post2 % l);
+		post1 = aft1 + pre1;
+		pre1 = post1 - pre1;
+		post2 = aft2 + pre2;
+		pre2 = post2 - pre2;
+	}
+	printf("\n");
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/test.c
@@ -0,0 +1,4 @@
+bra
+Bra
+bla
+ALI
