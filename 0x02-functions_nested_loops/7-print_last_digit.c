5b678d0f855483fbe8e598f956e3a024770b6d38
root <rickmwangi237@gmail.com>
2023-02-24 01:37:17 -0800

    add program to print every hour of the day

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+include "main.h"
+
+/**
+ * print_last_digit - prints last digit of integer
+ * @n: integer to be checked
+ * @a: placeholder for last digit
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+        int a = n % 10;
+
+        if (a < 0)
+        {
+            a = -a;
+            _putchar(a + '0');
+            return (a);
+        }
+        else
+        { 
+            _putchar(a + '0');
+            return (a);
+        }
+}
