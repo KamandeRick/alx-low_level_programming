f1d52f502439eeab4fa7015574c7cb1c9dd6e691
root <rickmwangi237@gmail.com>
2023-02-24 03:38:41 -0800

    fix syntax errors

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n = 98)
+	if (n == 98)
 	{
 		printf("%d ", n);
 	}
@@ -30,7 +30,7 @@ void print_to_98(int n)
 
 		}
 	}	
-	else if ( n <= 98 $$ n >= 0)
+	else if (( n <= 98) $$ (n >= 0))
 	{
 		for (i = n; i <= 98; i++)
 		{
