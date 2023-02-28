3aa6623a9b8c51d9d7a1496d24647ba4613c2431
root <rickmwangi237@gmail.com>
2023-02-28 14:47:45 -0800

    add function to return reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: Input string
+ *  * Return: String in reverse
+ */
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int incrmnt = 0;
+	int n;
+
+	while (s[incrmnt] != '\0')
+	counter++;
+	for (n = 0; n < incrmnt; n++)
+	{
+		incrmnt--;
+		rev = s[i];
+		s[i] = s[incrmnt];
+		s[incrmnt] = rev;
+	}
+}
+
