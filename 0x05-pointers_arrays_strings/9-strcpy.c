770fd16e1728c3b76ac075416515947373017fc0
root <rickmwangi237@gmail.com>
2023-02-28 15:21:42 -0800

    add funtion tocopy string being pointed to

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - copies the string pointed to by src
+ * including the terminating null byte
+ * @dest: destination to copy to
+ * @src: source to copy from
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
+
