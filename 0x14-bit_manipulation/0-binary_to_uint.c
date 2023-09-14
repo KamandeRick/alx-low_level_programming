31f296391b2e85e95ce5f4f449180f6b19042614
KamandeRick <rickmwangi237@gmail.com>
2023-09-14 10:43:32 -0700

    change variable name i to x

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,7 +6,7 @@
  * Return:new int converted from binary
  */
 
-unsigned int binary_to_uint(cons char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	int x;
 	unsigned int val = 0;
@@ -14,7 +14,7 @@ unsigned int binary_to_uint(cons char *b)
 	if (!b)
 		return (0);
 
-	for (x = 0; b[i]; x++)
+	for (x = 0; b[x]; x++)
 	{
 		if (b[x] < '0' || b[x] > '1')
 			return (0);
