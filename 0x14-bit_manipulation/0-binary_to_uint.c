e30a4c32175b01cecf34cbc5acba6daea6220baf
KamandeRick <rickmwangi237@gmail.com>
2023-09-14 10:35:12 -0700

    refactor code to remove compilation error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,23 +6,20 @@
  * Return:new int converted from binary
  */
 
-unsigned int binary_to_uint(const char *b)
-
+unsigned int binary_to_uint(cons char *b)
 {
-	unsigned int value = 0;
+	int x;
+	unsigned int val = 0;
 
 	if (!b)
 		return (0);
 
-	for (int x = 0; b[x]; x++)
+	for (x = 0; b[i]; x++)
 	{
-		if (b[x] != '0' && b[x] != '1')
+		if (b[x] < '0' || b[x] > '1')
 			return (0);
-
-		value = (value << x) | (b[x] - '0');
+		val = 2 * val + (b[x] - '0');
 	}
 
-	return (value);
+	return (val);
 }
-
-
