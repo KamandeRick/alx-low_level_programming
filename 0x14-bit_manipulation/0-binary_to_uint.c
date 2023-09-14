d1249efb8578c9c9c617eb5db17fc582aa188083
KamandeRick <rickmwangi237@gmail.com>
2023-09-14 06:36:51 -0700

    add function that converts binary number to unsigned integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - creates an int out of a binary number
+ * @b: binary number string
+ * Return:new int converted from binary 
+ */
+
+{
+	unsigned int value = 0;
+
+	if (!b) 
+		return (0);
+
+	for (int x = 0; b[i]; x++)
+	{
+		if (b[x] != '0' && b[x] != '1')
+			return (0);
+
+		value = (value << x) | (b[x] - '0');
+	}
+
+	return (value);
+}
