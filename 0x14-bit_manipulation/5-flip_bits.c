4b167f327a4666626682ce265704b1ec912769cb
KamandeRick <rickmwangi237@gmail.com>
2023-09-15 03:02:56 -0700

    change variable name

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -15,7 +15,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	for (x = 63; x >= 0; x--)
 	{
-		int_cur = int_excl >> i;
+		int_cur = int_excl >> x;
 		if (int_cur & 1)
 			count++;
 	}
